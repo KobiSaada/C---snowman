2722b5bfea7eb84cf581b5248a919faecd5d59f4
kobi <73976733+KobiSaada@users.noreply.github.com>
2021-03-08 23:47:38 +0200

    Update snowman.hpp

--- snowman.hpp
+++ snowman.hpp
@@ -18,27 +18,3 @@ bool all_chars_same(string testStr);
 };
 	
 
-//bool string_is_rectangular();
-  
-
-    /*Test if a string occupies a rectangular area when printed, i.e.,
-    all lines have the same length.
-    >>> string_is_rectangular('')
-    True
-    >>> string_is_rectangular('asdf')
-    True
-    >>> string_is_rectangular('asdf\\nqwer')
-    True
-    >>> string_is_rectangular('asdf\\nqwert')
-    False
-  */
-   // return all_equal(map(len, s.splitlines()))
-
-//int string_join_horizontal(strings, between=''):
-    /*Join a sequence of rectangular strings horizontally, preserving the
-    layout of each string. All strings must have the same number of lines.
-    Between two strings a column of `between` is inserted.
-    */
-  //  splitlines = str.splitlines
-    //return '\n'.join(between.join(lines)
-            //         for lines in zip(*map(splitlines, strings)))
